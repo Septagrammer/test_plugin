3076ab38a4f8e27b4182181e30caa6c7c12a8480
GitHub Action <action@github.com>
2022-01-19 19:36:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/septagrammer/testplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/septagrammer/testplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.septagrammer.testplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.septagrammer.testplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/septagrammer/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/septagrammer/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.septagrammer.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.septagrammer.testplugin.MyBundle
 
 class MyApplicationService {
 
